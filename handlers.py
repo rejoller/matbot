eb8875bc931c2a4e9c26adc38441a0e6702fb049
Your Name <you@example.com>
2024-05-19 23:46:02 +0700

    almost

--- handlers.py
+++ handlers.py
@@ -53,25 +53,49 @@ class User:
                 ZoneInfo("Asia/Krasnoyarsk")).strftime("%Y-%m-%d %H:%M:%S")}}},
             upsert=True
         )
+    
+    async def save_to_db(self):
+        await users_collection.update_one(
+            {"user_id": self.user_id},
+            {"$set": {"name": self.name, "balance": self.balance, "history": self.history}},
+            upsert=True
+        )
 
 tanya = User(user_id=374056328, name="Таня")
 masha = User(user_id=402748716, name="Маша")
+vlad = User(user_id=964635576, name="Влад")
 
 async def initialize_users():
+   # await tanya.save_to_db()
     await tanya.load_from_db(users_collection)
     await masha.load_from_db(users_collection)
+    await vlad.load_from_db(users_collection)
 
 
 
 
 
 
+@main_router.message(Command('add_user'))
+async def handle_payment(message: Message):
+    user_id = message.from_user.id
+    name = message.from_user.first_name
+    new_user = User(user_id=user_id, name=name)
+    
+    # Присваиваем переменной имя пользователя
+    globals()[name] = new_user
+    
+    # Для проверки можно вывести информацию о новом пользователе
+    print(f'Пользователь {name} создан с user_id: {user_id}')
+    await new_user.save_to_db()
+
+    await message.answer(f'Пользователь {name} создан с user_id: {user_id}')
 
 
 
 @main_router.message(Command('statistic'))
-async def handle_payment(message: Message):
-    print("handle_payment called")
+async def get_statistic(message: Message):
+    
     
 
 
@@ -144,7 +168,7 @@ async def handle_masha_mat(query: types.CallbackQuery):
     
     Balance: {masha_data['balance']}
     print(f'mashadata: {masha_data}')
-    await query.message.answer_animation(animation = 'CgACAgIAAxkBAAMpZkcNoLLY6L4DiIdeDjc1gMa6dFUAAjlEAAL0uzlKemq4aa2FFig1BA', caption=masha_message, parse_mode='HTML')
+    await query.message.edit_caption(animation = 'CgACAgIAAxkBAANVZkcSILbeKabcnkR4YR4j2Jl8BuoAAoFEAAL0uzlKcwwmpVIVQWU1BA', caption=masha_message, parse_mode='HTML')
 
 
 
@@ -156,14 +180,14 @@ async def handle_masha_mat(query: types.CallbackQuery):
     db = mongodb_client.mat_db  # Название вашей базы данных
     users_collection = db.users
     amount = 10
-    await masha.update_balance(amount, users_collection)
-    masha_data = await users_collection.find_one({"user_id": 374056328})
-    balance = masha_data.get('balance', 'Неизвестно')
+    await tanya.update_balance(amount, users_collection)
+    tanya_data = await users_collection.find_one({"user_id": 374056328})
+    balance = tanya_data.get('balance', 'Неизвестно')
 
     # Формирование сообщения
-    masha_message = f"<b>Мат зафиксирован!</b> \nШтрафы Тани: {balance} р."
+    tanya_message = f"<b>Мат зафиксирован!</b> \nШтрафы Тани: {balance} р."
     
     
-    Balance: {masha_data['balance']}
-    print(f'mashadata: {masha_data}')
-    await query.message.answer_animation(animation = 'CgACAgIAAxkBAAMpZkcNoLLY6L4DiIdeDjc1gMa6dFUAAjlEAAL0uzlKemq4aa2FFig1BA', caption=masha_message, parse_mode='HTML')
+    Balance: {tanya_data['balance']}
+    print(f'mashadata: {tanya_data}')
+    await query.message.edit_caption(animation = 'CgACAgIAAxkBAANVZkcSILbeKabcnkR4YR4j2Jl8BuoAAoFEAAL0uzlKcwwmpVIVQWU1BA', caption=tanya_message, parse_mode='HTML')
